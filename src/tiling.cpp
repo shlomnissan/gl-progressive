53b70bebc2fe75dfec277e2529cf7f640e21b50e
Shlomi Nissan <admin@betamark.com>
2025-05-05 08:42:34 -0700

    Add orthographic camera

--- src/tiling.cpp
+++ src/tiling.cpp
@@ -6,7 +6,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include "core/geometry.h"
+#include "core/orthographic_camera.h"
 #include "core/shaders.h"
 #include "core/window.h"
 
@@ -19,6 +19,7 @@ auto main() -> int {
     const auto win_width = 1024;
     const auto win_height = 768;
     auto window = Window {win_width, win_height, "Tiling"};
+    auto camera = OrthographicCamera {0.0f, win_width, win_height, 0.0f, -1.0f, 1.0f};
 
     auto shader = Shaders {{
         {ShaderType::kVertexShader, _SHADER_scene_vert},
@@ -27,25 +28,9 @@ auto main() -> int {
 
     glEnable(GL_DEPTH_TEST);
 
-    auto updateProjection = [&shader](int width, int height) {
-        auto ratio = static_cast<float>(width) / static_cast<float>(height);
-        shader.SetUniform("Projection", glm::perspective(45.0f, ratio, 0.1f, 100.0f));
-    };
-
-    updateProjection(win_width, win_height);
-    window.on_resize([&updateProjection](const int width, const int height){
-        updateProjection(width, height);
-    });
-
-    auto view = glm::lookAt(
-        glm::vec3{0.0f, 0.0f, 1.0f},
-        glm::vec3{0.0f, 0.0f, 0.0f},
-        glm::vec3{0.0f, 1.0f, 0.0f}
-    );
-
     auto tile = PlaneGeometry {{
-        .width = 1.0f,
-        .height = 1.0f,
+        .width = 512.0f,
+        .height = 512.0f,
         .width_segments = 1,
         .height_segments = 1
     }};
@@ -55,10 +40,14 @@ auto main() -> int {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         auto model = glm::mat4{1.0f};
-        model = glm::scale(model, {0.3f, 0.3f, 0.3f});
+        model = glm::translate(model, glm::vec3 {
+            win_width >> 1,
+            win_height >> 1,
+            0.0f
+        });
 
-        shader.SetUniform("ModelView", view * model);
-
+        shader.SetUniform("Projection", camera.Projection());
+        shader.SetUniform("ModelView", model);
         tile.Draw(shader);
     });
 
