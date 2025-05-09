26a4fac4ea1868334e95b5354d1ddcad2b65aa7c
Shlomi Nissan <admin@betamark.com>
2025-05-09 07:10:04 -0700

    Add a chunk manager class

---
+++ src/chunk_manager.cpp
@@ -0,0 +1,4 @@
+// Copyright © 2024 - Present, Shlomi Nissan.
+// All rights reserved.
+
+#include "chunk_manager.h"
---
+++ src/chunk_manager.h
@@ -0,0 +1,10 @@
+// Copyright © 2024 - Present, Shlomi Nissan.
+// All rights reserved.
+
+#pragma once
+
+#include "chunk.h"
+
+class ChunkManager {
+
+};
